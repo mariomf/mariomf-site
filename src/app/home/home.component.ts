fbbe6428a0b23817bf58bd250625cd5bc98ee49a
mario.mucino.flores <mario.mucino.flores@gmail.com>
2024-02-26 23:32:03 +0000

    fix: primeng

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { CarouselModule } from 'primeng/carousel';
 import { ImageModule } from 'primeng/image';
 import { ButtonModule } from 'primeng/button';
 import { FieldsetModule } from 'primeng/fieldset';
 import { CardModule } from 'primeng/card';
+import { AccordionModule } from 'primeng/accordion';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { BadgeModule } from 'primeng/badge';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CarouselModule,
     ButtonModule,
     ImageModule,
     FieldsetModule,
-    CardModule
+    CardModule,
+    AccordionModule,
+    AvatarModule,
+    BadgeModule,
+    AvatarGroupModule
   ], 
   standalone: true
 })
